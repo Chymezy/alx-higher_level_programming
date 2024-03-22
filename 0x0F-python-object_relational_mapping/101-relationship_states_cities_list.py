509ef7d8f579151dcbdf65f1d26fdb4806b471a8
Chymezy <chymezy332@gmail.com>
2024-03-22 14:41:22 -0700

    removed guide at the top

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
+"""
+script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
-""" lists all State objects, and corresponding City objects,
-contained in the database """
 
 if __name__ == "__main__":
-
-    import sys
-    from sqlalchemy import create_engine
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy.orm import sessionmaker
-    from relationship_city import City
-    from relationship_state import Base, State
-
-    inp = sys.argv
-    if len(inp) < 4:
-        exit(1)
-
-    conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
-    engine = create_engine(conn_str.format(inp[1], inp[2], inp[3]))
-    Session = sessionmaker(bind=engine)
-
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    db_url = db_url.format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(db_url)
     Base.metadata.create_all(engine)
-
+    Session = sessionmaker(bind=engine)
     session = Session()
-    my_query = session.query(State) \
-                      .order_by(State.id) \
-                      .all()
-    for state in my_query:
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
